ea66c926297266e3a149baf49a71180714f9cd5f
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-07-08 11:32:05 +0700

    using conts

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const card = [];
 for (let i = 1; i <= 8; i++) {
     card[i] = document.querySelector(`.card-${i}`);
-    let a = i+1;
+    const a = i+1;
     card[i].style.animation=`spawn 0.${a}s ease-in-out`;
 }
